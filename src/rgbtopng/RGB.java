09efe4e9815ad04bb49a204bb8479d38f1b864d4
David Fink <david@davidfink.me>
2016-04-01 01:15:14 -0400

    Fixed Incorrect Author Statement
    Replaced "david" with proper author name ("David Fink")

--- src/rgbtopng/RGB.java
+++ src/rgbtopng/RGB.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2016 david.
+ * Copyright 2016 David Fink
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
